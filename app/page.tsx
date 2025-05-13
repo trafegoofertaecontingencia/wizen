acec70e0751e3f3043f4575a2e599fa8795b2f91
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-13 15:23:10 -0300

    centralização dos textos

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <main>
-        <div className="pt-15 flex justify-center">
-          <div className="flex flex-col">
-            <p className="text-3xl">WIZEN</p>
-            <p className="text-blue-300 text-4xl font-bold">App mobile</p>
-            <p className="text-5xl font-bold">Seu novo assistente <br /> financeiro chegou</p>
-            <p>Organize sua vida financeira com inteligência - mesmo que viva no aperto</p>
+        <div className="pt-15 flex justify-center text-center">
+          <div className="flex flex-col items-center">
+            <p className="text-2xl">WIZEN</p>
+            <p className="text-blue-300 text-3xl font-bold">App mobile</p>
+            <p className="text-4xl font-bold">Seu novo assistente <br /> financeiro chegou</p>
+            <p className="m-2">Organize sua vida financeira com inteligência - mesmo que viva no aperto</p>
             <button className="bg-blue-300 rounded-lg p-3 mt-2 w-50">Testar por 7 dias grátis</button>
           </div>
         </div>
