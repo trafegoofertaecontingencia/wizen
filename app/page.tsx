353876baeee084137b039a4817b12fdbc4005fdb
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-13 14:32:22 -0300

    botão adicionado

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ export default function Home() {
     <div className="min-h-screen">
       <main>
         <div className="pt-15 pl-15">
-          <div>
-            <p>WIZEN</p>
-            <p className="text-blue-500">App mobile</p>
-            <p>Seu novo assistente financeiro chegou</p>
+          <div className="flex flex-col">
+            <p className="text-3xl">WIZEN</p>
+            <p className="text-blue-300 text-4xl font-bold">App mobile</p>
+            <p className="text-5xl font-bold">Seu novo assistente <br /> financeiro chegou</p>
+            <p>Organize sua vida financeira com inteligência - mesmo que viva no aperto</p>
+            <button className="bg-blue-300 rounded-lg p-3 mt-2 w-50">Testar por 7 dias grátis</button>
           </div>
         </div>
       </main>
