f56b0befb8a8cb4c903a51d59bcf3cf991ae0ec3
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-13 16:47:28 -0300

    resolução do problema no building

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="h-24 bg-[#333] flex justify-center items-center text-white">
-            <p>&copy; {new Date().getFullYear()} Wizen. Todos os direitos reservados.</p>
+            &copy; {new Date().getFullYear()} Wizen. Todos os direitos reservados.
         </footer>
     )
 }
