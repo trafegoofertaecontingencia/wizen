a230b41039de8fac793e1fcdc77d93625519c5a0
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-14 11:08:45 -0300

    ajustes na sessão solution

--- app/components/Solution.tsx
+++ app/components/Solution.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Solution() {
     return (
         <div className="flex flex-col items-center mt-15">
-            <h1 className="text-2xl uppercase font-bold">Você vive no limite e nunca sobra nada? A culpa não é só sua</h1>
+            <h1 className="text-2xl uppercase font-bold w-[600px] text-center m-6 mt-0">Você vive no limite e nunca sobra nada? A culpa não é só sua</h1>
             <p>Mas a solução pode estar no seu bolso, literalmente!</p>
             <Image
                 src="/images/laptop_and_phone.png" 
