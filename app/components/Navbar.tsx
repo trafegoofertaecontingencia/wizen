4eb8ed814d8e1fa5bf29d39e262ce42257d6970c
trafegoofertaecontingencia <juniorcms95@gmail.com>
2025-05-16 10:52:48 -0300

    navbar funcionando 100%

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,18 +18,19 @@ export default function Navbar() {
     console.log(isOpen);
 
     return (
-        <nav className="relative overflow-hidden h-8 bg-[#333]">
+        <nav className="relative overflow-hidden bg-[#333]">
             <ul className="md:flex gap-3 pl-3 pr-3 pb-2 text-white hidden">
                 <li><Link href="/">Início</Link></li>
                 <li><Link href="/">Contato</Link></li>
                 <li><Link href="/">Sobre nós</Link></li>
             </ul>
+
             <motion.div 
-            initial={{ height: "auto" }}
-            animate={isOpen ? {height: "200px"} : {height: "auto"}}
+            initial={{ height: "24px" }}
+            animate={isOpen ? {height: "200px"} : {height: "24px"}}
             transition={isOpen ? {} : {duration: 0.4, delay: 0.2}}
             className="bg-[#333] text-white cursor-pointer md:hidden" onClick={handleMenu}>
-            <GiHamburgerMenu onClick={handleMenu} />
+            <GiHamburgerMenu className="ml-3" onClick={handleMenu} />
             </motion.div>
 
             <motion.div className="bg-[#333] w-[100%] p-3 absolute right-[-100%] z-1 top-[50px] text-white"
